5cf7c0d1c4dd9cb64c33e7a00cfefbabe0227806
syedumairh1998 <142947133+syedumairh1998@users.noreply.github.com>
2024-07-30 13:12:43 +0500

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,15 @@
+* CREATE TABLE personal(
+id INT,
+NAME  varchar(50),
+birth_date DATE ,
+phone VARCHAR(12),
+gender VARCHAR(1));student
+
+==========================
+
+*CREATE table product(
+pid INT,
+pname VARCHAR(50),
+pcompnay VARCHAR(50),
+price INT 
+);
